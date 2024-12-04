ea6b52a386c6dda75ebc7b7f93ee574922c4f05f
root <root@ip-172-31-32-87.eu-north-1.compute.internal>
2024-12-04 00:49:16 +0000

    Initial commit - Dockerized app

--- mern/frontend/src/components/RecordList.jsx
+++ mern/frontend/src/components/RecordList.jsx
@@ -41,7 +41,7 @@ export default function RecordList() {
   // This method fetches the records from the database.
   useEffect(() => {
     async function getRecords() {
-      const response = await fetch(`http://localhost:5050/record/`);
+      const response = await fetch(`http://localhost:5050/record`);
       if (!response.ok) {
         const message = `An error occurred: ${response.statusText}`;
         console.error(message);
